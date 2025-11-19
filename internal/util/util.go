9db8879cb0e2208d30bfedb8ddc868ac01f35d61
Madjim <maxbabintsev@gmail.com>
2025-11-19 23:08:17 +0300

    feat: создана Save() feat: создана ValidFIO() feat: создана ValidSpec() feat: создана ValidDate() refactor: введены глобальные переменные

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,1 +1,47 @@
 package util
+
+import (
+	"bufio"
+	"fmt"
+	"time"
+)
+
+var (
+	base = []UserCard{}
+	name string
+	spec string
+	date time.Time
+	err  error
+)
+
+func Save(scanner *bufio.Scanner) error {
+	if !scanner.Scan() {
+		return scanner.Err()
+	}
+
+	fmt.Print("ФИО: ")
+	name, err = ValidFIO(scanner.Text())
+	if err != nil {
+		return err
+	}
+
+	if !scanner.Scan() {
+		return scanner.Err()
+	}
+
+	fmt.Print("Специальность врача: ")
+	spec, err = ValidSpec(scanner.Text())
+	if err != nil {
+		return err
+	}
+
+	fmt.Print("Введите дату посещения в формате \"YYYY-MM-DD\": ")
+	date, err = ValidDate(scanner.Text())
+	if err != nil {
+		return err
+	}
+
+	base = append(base, UserCard{Name: name, Specialization: spec, Date: date})
+
+	return nil
+}
--- internal/util/validations.go
+++ internal/util/validations.go
@@ -1,1 +1,49 @@
 package util
+
+import (
+	"errors"
+	"strings"
+	"time"
+	"unicode"
+)
+
+var (
+	errEmptyLine = errors.New("empty line")
+	text         string
+	runes        []rune
+	parts        []string
+	replaser     = strings.NewReplacer(" ", "-", ".", "-", "\\", "-", "/", "-")
+)
+
+func ValidFIO(str string) (string, error) {
+	text, err = ValidSpec(str)
+
+	if err != nil {
+		return text, err
+	}
+
+	parts = strings.FieldsFunc(text, unicode.IsSpace)
+
+	for i, p := range parts {
+		runes = []rune(p)
+		runes[0] = unicode.ToUpper(runes[0])
+		parts[i] = string(runes)
+	}
+
+	return strings.Join(parts, " "), nil
+}
+
+func ValidSpec(str string) (string, error) {
+	if str == "" {
+		return "", errEmptyLine
+	}
+
+	text = strings.TrimSpace(str)
+	return text, nil
+}
+
+func ValidDate(str string) (time.Time, error) {
+	text = replaser.Replace(str)
+	date, err := time.Parse("2006-01-02", str)
+	return date, err
+}
